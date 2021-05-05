3e29aa392daa772c3b89ff8a83f9df3eee7cdb12
deepakchaturvedifzd <61619479+deepakchaturvedifzd@users.noreply.github.com>
2021-05-05 20:56:03 +0530

    union

---
+++ Languages/C++/Union in C++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+union Test
+{
+    int x;
+    int y;
+};
+int main()
+{
+    Test t;
+
+    t.x=10;
+    cout<<t.x<<" "<<t.y<<endl;
+    t.y=2005;
+    cout<<t.x<<" "<<t.y<<endl;
+
+    return 0;
+}
